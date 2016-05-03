ffc6d63b2b6ddeb059c55875e60ccc1637a653b3
Jens X Augustsson <jens.augustsson@redpill-linpro.com>
2016-05-03 17:00:41 +0200

    Downward fix for multiple yAxis

--- scalable-yaxis.js
+++ scalable-yaxis.js
@@ -9,7 +9,7 @@
  */
 
 // JSLint options:
-/*global Highcharts, document */
+/*global Highcharts, document, console */
 
 (function (H) {
     'use strict';
@@ -25,7 +25,15 @@
 
         var chart = this,
             renderer = chart.renderer,
-            yAxes = chart.yAxis;
+            yAxes = chart.yAxis,
+            animation = yAxes.length <= 2 ? false : { duration: 500},
+            offsetOpposite = 0,
+            offsetAdjacent = 0,
+            mousedownYAxis,
+            previousYPixels,
+            previousMin,
+            previousMax,
+            isUpperPortion;
 
         each(yAxes, function (yAxis, index) {
             var options = yAxis.options,
@@ -41,20 +49,29 @@
                 downYValue;
 
             if (scalable) {
-                bBoxWidth = 40;
+                bBoxWidth = 60;
                 bBoxHeight = chart.containerHeight - yAxis.top - yAxis.bottom;
                 bBoxX = yAxis.opposite ? (labels.align === 'left' ? chart.containerWidth - yAxis.right : chart.containerWidth - (yAxis.right + bBoxWidth)) : (labels.align === 'left' ? yAxis.left : yAxis.left - bBoxWidth);
-                bBoxX += index * bBoxWidth;
+                //Handle offset for multiple possible opposite yAxes
+                if (yAxis.opposite) {
+                    bBoxX += offsetOpposite;
+                    offsetOpposite += bBoxWidth;
+                } else {
+                    bBoxX -= offsetAdjacent;
+                    offsetAdjacent += bBoxWidth;
+                }
+                yAxis.jensIndex = index;
+
                 bBoxY = yAxis.top;
 
                 // Render an invisible bounding box around the y-axis label group
                 // This is where we add mousedown event to start dragging
                 labelGroupBBox = renderer.rect(bBoxX, bBoxY, bBoxWidth, bBoxHeight)
                     .attr({
-                        fill: '#000',
-                        stroke: 'red',
-                        'stroke-width': 2,
-                        opacity: 0.5,
+                        fill: '#fff',
+                        /*stroke: 'red',
+                        'stroke-width': 1,*/
+                        opacity: 0.0,
                         zIndex: 8
                     })
                     .css({
@@ -69,16 +86,23 @@
 
                     downYValue = yAxis.toValue(downYPixels);
                     isDragging = true;
+                    mousedownYAxis = yAxis;
+                    previousYPixels = downYPixels;
+                    //previousMax = mousedownYAxis.toValue(downYPixels);
+                    previousMin = mousedownYAxis.getExtremes().dataMin;
+                    previousMax = mousedownYAxis.getExtremes().dataMax;
+                    isUpperPortion = mousedownYAxis.toValue(downYPixels) > (mousedownYAxis.getExtremes().dataMin + mousedownYAxis.getExtremes().dataMax) / 2;
+                    console.log('isUpperPortion: ' + isUpperPortion);
                 });
 
                 addEvent(chart.container, 'mousemove', function (e) {
                     if (isDragging) {
-                        body.style.cursor = 'ns-resize';
+                        doc.body.style.cursor = 'ns-resize';
 
                         var dragYPixels = chart.pointer.normalize(e).chartY,
-                            dragYValue = yAxis.toValue(dragYPixels),
+                            dragYValue = mousedownYAxis.toValue(dragYPixels),
 
-                            extremes = yAxis.getExtremes(),
+                            extremes = mousedownYAxis.getExtremes(),
                             userMin = extremes.userMin,
                             userMax = extremes.userMax,
                             dataMin = extremes.dataMin,
@@ -88,27 +112,43 @@
                             max = userMax !== undefined ? userMax : dataMax,
 
                             newMin,
-                            newMax;
+                            newMax,
 
-                        // update max extreme only if dragged from upper portion
-                        // update min extreme only if dragged from lower portion
-                        if (downYValue > (dataMin + dataMax) / 2) {
+                            isDownward = (dragYPixels - previousYPixels > 0 ? true : false),
+                            deltaValue = dragYValue - downYValue;
+
+                        console.log('isDownward: ' + isDownward);
+
+                        if (isUpperPortion) {
+                            // update max extreme only if dragged from upper portion
+                            //newMax = max - (dragYValue - downYValue);
+                            newMax = isDownward ? Math.min(previousMax, max - deltaValue) : Math.max(previousMax, max - deltaValue);
+                            //console.log('deltadrag: ' + (dragYValue - downYValue));
+                            //newMax = newMax > dataMax ? newMax : dataMax; //limit
                             newMin = min;
-                            newMax = max - (dragYValue - downYValue);
-                            newMax = newMax > dataMax ? newMax : dataMax; //limit
                         } else {
+                            // update min extreme only if dragged from lower portion
                             newMin = min - (dragYValue - downYValue);
                             newMin = newMin < dataMin ? newMin : dataMin; //limit
                             newMax = max;
                         }
-
-                        yAxis.setExtremes(newMin, newMax, true, false);
+                        if (newMax !== previousMax || newMin !== previousMax) {
+                            console.log('dragYPixels: ' + dragYPixels + ', dragYValue: ' + dragYValue + ', newMax: ' + newMax);
+                            mousedownYAxis.setExtremes(newMin, newMax, true, animation);
+                            //Remember these
+                            previousYPixels = dragYPixels;
+                            previousMin = newMin;
+                            previousMax = newMax;
+                        } else {
+                            console.log('Ignoring newMax ' + newMax + ' and newMin ' + newMin);
+                        }
                     }
                 });
 
                 addEvent(document, 'mouseup', function () {
-                    body.style.cursor = 'default';
+                    doc.body.style.cursor = 'default';
                     isDragging = false;
+                    mousedownYAxis = null;
                 });
 
                 // double-click to go back to default range

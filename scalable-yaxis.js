5a4c7bfb69c81459c2d1f7376707f2abbf64de63
Jempa <jens@augustsson.eu>
2016-05-10 09:24:57 +0200

    Scaling of axes can now be synched. Major cleanup.

--- scalable-yaxis.js
+++ scalable-yaxis.js
@@ -9,7 +9,7 @@
  */
 
 // JSLint options:
-/*global Highcharts, document, console */
+/*global Highcharts, document */
 
 (function (H) {
     'use strict';
@@ -26,14 +26,11 @@
         var chart = this,
             renderer = chart.renderer,
             yAxes = chart.yAxis,
-            animation = yAxes.length <= 2 ? false : { duration: 500},
+            //animation = yAxes.length <= 2 ? false : { duration: 250}, //Enables smoother but lagging redraws
+            animation = false,
+            synchYAxes = true,
             offsetOpposite = 0,
-            offsetAdjacent = 0,
-            mousedownYAxis,
-            previousYPixels,
-            previousMin,
-            previousMax,
-            isUpperPortion;
+            offsetAdjacent = 0;           
 
         each(yAxes, function (yAxis, index) {
             var options = yAxis.options,
@@ -48,22 +45,22 @@
                 isDragging = false,
                 downYPixels,
                 downYValue,
-                downMax;
+                isUpperPortion;
 
             if (scalable) {
                 bBoxWidth = 60;
                 bBoxHeight = chart.containerHeight - yAxis.top - yAxis.bottom;
                 bBoxX = yAxis.opposite ? (labels.align === 'left' ? chart.containerWidth - yAxis.right : chart.containerWidth - (yAxis.right + bBoxWidth)) : (labels.align === 'left' ? yAxis.left : yAxis.left - bBoxWidth);
-                //Handle offset for multiple possible opposite yAxes
+                //Handle offset for multiple yAxes
                 if (yAxis.opposite) {
-                    bBoxX += offsetOpposite;
-                    offsetOpposite += bBoxWidth;
+                	bBoxX += offsetOpposite;
+                	offsetOpposite += bBoxWidth;
                 } else {
-                    bBoxX -= offsetAdjacent;
-                    offsetAdjacent += bBoxWidth;
+                	bBoxX -= offsetAdjacent;
+                	offsetAdjacent += bBoxWidth;                	
                 }
-                yAxis.jensIndex = index;
-
+                yAxis.index = index;
+                
                 bBoxY = yAxis.top;
 
                 // Render an invisible bounding box around the y-axis label group
@@ -71,9 +68,13 @@
                 labelGroupBBox = renderer.rect(bBoxX, bBoxY, bBoxWidth, bBoxHeight)
                     .attr({
                         fill: '#fff',
-                        /*stroke: 'red',
-                        'stroke-width': 1,*/
                         opacity: 0.0,
+                        /*
+                        fill: '#aaa',
+                        stroke: 'red',
+                        'stroke-width': 1,
+                        opacity: 0.25,
+                        */                        
                         zIndex: 8
                     })
                     .css({
@@ -84,101 +85,70 @@
                 labels.style.cursor = 'ns-resize';
 
                 addEvent(labelGroupBBox.element, 'mousedown', function (e) {
-                    isDragging = true;
-                    mousedownYAxis = yAxis;
-
+                    isDragging = true;                	
                     downYPixels = pointer.normalize(e).chartY;
                     downYValue = yAxis.toValue(downYPixels);
-                    downMax = mousedownYAxis.max;
-                    //downMax = mousedownYAxis.getExtremes().userMax || mousedownYAxis.getExtremes().dataMax;
-
-
-                    previousYPixels = downYPixels;
-                    previousMin = mousedownYAxis.getExtremes().userMin || mousedownYAxis.getExtremes().dataMin;
-                    previousMax = mousedownYAxis.getExtremes().userMax || mousedownYAxis.getExtremes().dataMax;
-
-                    isUpperPortion = mousedownYAxis.toValue(downYPixels) > (mousedownYAxis.getExtremes().dataMin + mousedownYAxis.getExtremes().dataMax) / 2;
-                    console.log('isUpperPortion: ' + isUpperPortion);
+                    isUpperPortion = yAxis.toValue(downYPixels) > (yAxis.getExtremes().dataMin + yAxis.getExtremes().dataMax) / 2;
                 });
 
                 addEvent(chart.container, 'mousemove', function (e) {
+                	var selectedAxes = synchYAxes ? yAxes : [yAxis],
+            			dragYPixels = chart.pointer.normalize(e).chartY,
+                        dragYValue,
+                        extremes,
+                        userMin,
+                        userMax,
+                        dataMin,
+                        dataMax,
+                        min,
+                        max,
+                        newMin,
+                        newMax,
+                        index;
+                	
                     if (isDragging) {
-                        doc.body.style.cursor = 'ns-resize';
+                    	doc.body.style.cursor = 'ns-resize';
 
-                        var dragYPixels = chart.pointer.normalize(e).chartY,
-                            dragYValue = mousedownYAxis.toValue(dragYPixels),
+                    	for (index in selectedAxes) {
+                            dragYValue = selectedAxes[index].toValue(dragYPixels);
+                            extremes = selectedAxes[index].getExtremes();
+                            userMin = extremes.userMin;
+                            userMax = extremes.userMax;
+                            dataMin = extremes.dataMin;
+                            dataMax = extremes.dataMax;
 
-                            extremes = mousedownYAxis.getExtremes(),
-                            userMin = extremes.userMin,
-                            userMax = extremes.userMax,
-                            dataMin = extremes.dataMin,
-                            dataMax = extremes.dataMax,
-                            min = userMin !== undefined ? userMin : dataMin,
-                            max = userMax !== undefined ? userMax : dataMax,
-                            newMin,
-                            newMax,
-                            isDownward = (dragYPixels - previousYPixels > 0 ? true : false),
-                            //deltaValue = dragYValue - downYValue;
-                            deltaValue = mousedownYAxis.toValue(dragYPixels) - downYValue;
-
-                            //previousMin = previousMin || min;
-                            //previousMax = previousMax || max;
-
-                        console.log('deltaPixelDown ' + (dragYPixels - downYPixels) + ', deltaPixelPrevious ' + (dragYPixels - previousYPixels));
-                        //console.log('isDownward: ' + isDownward + ' with deltaValue ' + deltaValue);
-
-                        if (Math.abs((dragYPixels - previousYPixels)) > 0) {
-                            //Movement detected
-                            //console.log('Previous max ' + previousMax + ', min ' + previousMin);
-                            if (isUpperPortion) {
-
-                                // update max extreme only if dragged from upper portion
-                                //newMax = max - (dragYValue - downYValue);
-                                //newMax = mousedownYAxis.getExtremes().dataMax - deltaValue;
-                                newMax = mousedownYAxis.max - deltaValue;
-                                console.log('deltaValue ' + deltaValue + ', newMax ' + newMax);
-                                //newMax = isDownward ? Math.max(previousMax, max - deltaValue) : Math.min(previousMax, max - deltaValue);
-                                //console.log('deltadrag: ' + (dragYValue - downYValue));
-                                //newMax = newMax > dataMax ? newMax : dataMax; //limit
-                                newMin = min;
-    //                            newMax = max - (dragYValue - downYValue);
-    //                            newMax = newMax > dataMax ? newMax : dataMax; //limit
-                            } else {
-                                // update min extreme only if dragged from lower portion
-                                newMin = isDownward ? Math.min(previousMin, min - deltaValue) : Math.max(previousMin, min - deltaValue);
-                                //newMin = min - (dragYValue - downYValue);
-                                //newMin = newMin < dataMin ? newMin : dataMin; //limit
-                                newMax = max;
-                            }
-//                            if (newMax !== previousMax || newMin !== previousMin) {
-                                //console.log('setExtremes for deltaValue: ' + deltaValue + ', newMin ' + newMin + ', newMax ' + newMax);
-                                //mousedownYAxis.max = newMax;
-                                mousedownYAxis.update({max: newMax});
-                                //mousedownYAxis.setExtremes(newMin, newMax, true, false);
-                                //Remember these
-                                previousYPixels = dragYPixels;
-                                previousMin = newMin;
-                                previousMax = newMax;
-//                            } else {
-//                                //console.log('Ignoring extremes for deltaValue ' + deltaValue + ' with newMax ' + newMax + ' and newMin ' + newMin);
-//                            }
-                        }
+                            min = userMin !== undefined ? userMin : dataMin;
+                            max = userMax !== undefined ? userMax : dataMax;
+	                            
+	                        if (isUpperPortion) {
+	                            // update max extreme only if dragged from upper portion
+	                        	newMax = max - (dragYValue - downYValue);                            	
+	                        	//newMax = newMax > dataMax ? newMax : dataMax; //limit //disabled
+	                            newMin = min;
+	                        } else {
+	                            // update min extreme only if dragged from lower portion
+	                            newMin = min - (dragYValue - downYValue);
+	                            //newMin = newMin < dataMin ? newMin : dataMin; //limit //disabled
+	                            newMax = max;
+	                        }                        
+	                        selectedAxes[index].setExtremes(newMin, newMax, true, animation);
+                    	}
                     }
                 });
 
                 addEvent(document, 'mouseup', function () {
-                    doc.body.style.cursor = 'default';
+                	doc.body.style.cursor = 'default';
                     isDragging = false;
-                    mousedownYAxis = null;
                 });
 
                 // double-click to go back to default range
                 addEvent(labelGroupBBox.element, 'dblclick', function () {
-                    var extremes = yAxis.getExtremes(),
-                        dataMin = extremes.dataMin,
-                        dataMax = extremes.dataMax;
-
-                    yAxis.setExtremes(dataMin, dataMax, true, false);
+                	var selectedAxes = synchYAxes ? yAxes : [yAxis],
+                			index;
+                	
+                	for (index in selectedAxes) {
+                		selectedAxes[index].setExtremes(selectedAxes[index].getExtremes().dataMin, selectedAxes[index].getExtremes().dataMax, true, false);                		
+                	}
                 });
             }
         });
